ccb44571fa5eda7a15bf148d4b7c7075289ef8f4
zeinabsayyadi <zeinsa0158@gmail.com>
2022-03-31 00:48:38 +0430

    login page added

---
+++ js/login.js
@@ -0,0 +1,11 @@
+let phoneInput = document.querySelector('[name="phone"]');
+let phoneNumInptbtn = document.querySelector('#phoneNumInptbtn');
+const errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
+
+//create form-2
+let codeInterForm = document.createElement('div');
+phoneNumInptbtn.addEventListener("click",(e)=>{
+    //validate number
+   //sms a 6-dig number to the phone number
+   //toggle forms
+})
