e198ca82e650d921f192670a35f1506e0c2d1bc1
zeinabsayyadi <zeinsa0158@gmail.com>
2022-04-09 00:04:16 +0430

    login.js updated

--- js/login.js
+++ js/login.js
@@ -1,27 +1,78 @@
-import * as intlTelInput from "../node_modules/intl-tel-input/build/js/intlTelInput.js"
 
-//first form
+'use strict';
+
+//initial elements
+
 let loginPart = document.querySelector('.loginPart');
-let loginTitrP = document.querySelector('.loginTitrPart');
-let phonenumForm = document.querySelector('.phonenumForm');
-let phoneInput = document.querySelector('.phone');
 
-let phNResponseMsg = document.querySelector('.phNResponseMsg');
-phNResponseMsg.style = "transition = all 5s";
+//________create form-1________
+
+let loginTitrP = document.createElement('div');
+loginTitrP.className = "pb-5 loginTitrPart";
+
+let loginTitr = document.createElement('p');
+loginTitr.className = "loginTitr";
+loginTitr.style = "color: var(--bs-primary-200)";
+loginTitr.innerHTML = "لطفا برای ورود شماره تلفن خود را وارد کنید";
+
+let titrHr = document.createElement('hr');
+titrHr.className = "background-color: var(--bs-primary-200)";
+
+let phoneNumForm = document.createElement('div');
+phoneNumForm.className = "col-8 phoneNumForm";
+phoneNumForm.style = "width: min-content;"
+
+let phoneNumTag = document.createElement('p');
+phoneNumTag.className = "my-2 fw-medium fw-medium phonenNumTag";
+phoneNumTag.style = "font-size: 1.125rem; color : var(-bs-natural-300)";
+phoneNumTag.innerHTML = "شماره تماس";
+
+let phoneNumFormForm = document.createElement('form');
+phoneNumFormForm.className = "d-flex flex-column align-items-center";
+phoneNumFormForm.style = "direction: ltr";
+
+let phoneInput = document.createElement('input');
+phoneInput.className = "border-1 bg-transparent col-12 formsStyle";
+phoneInput.style = "border-color: var(--bs-primary-200)";
+phoneInput.type="tel";
+phoneInput.placeholder="09123456789";
+
+
+let phNResponseMsg = document.createElement('div');
+
 let errorMsg = document.createElement('span');
+errorMsg.style = "color : var(--bs-warning-100)";
 
 let validMsg = document.createElement('span');
+validMsg.style = "color : var(--bs-secondary-200)";
 
-let phoneNumInptbtn = document.querySelector('.phoneNumInptbtn');
+let phoneNumInptbtn = document.createElement('button');
+phoneNumInptbtn.className = "phoneNumInptbtn border-0 my-4 align-items-center formsStyle";
+phoneNumInptbtn.style="background-color: var(--bs-secondary-100); color : var(-bs-natural-300)";
+phoneNumInptbtn.type="submit";
+phoneNumInptbtn.innerHTML = "وارد شوید";
 
-//create form-2
+loginTitrP.appendChild(loginTitr);
+loginTitrP.appendChild(titrHr);
+
+phoneNumForm.appendChild(phoneNumTag);
+
+phoneNumFormForm.appendChild(phoneInput);
+phoneNumFormForm.appendChild(phNResponseMsg);
+phoneNumFormForm.appendChild(phoneNumInptbtn);
+phoneNumForm.appendChild(phoneNumFormForm);
+
+loginPart.appendChild(loginTitrP);
+loginPart.appendChild(phoneNumForm);
+
+//_______create form-2_________
 
 let codeInter = document.createElement('div');
 codeInter.className = "col-8";
 
 let codeInterTitle = document.createElement('p');
 codeInterTitle.className = "text-end my-2 me-lg-5 pe-lg-5";
-codeInterTitle.style = "ont-size: 1.125rem;";
+codeInterTitle.style = "font-size: 1.125rem; color : var(-bs-natural-300)";
 codeInterTitle.innerHTML = `لطفا کد ارسالی به شماره تلفن 09123456789 را وارد کنید`
 
 let codeInterEditeNumField = document.createElement('p');
@@ -34,23 +85,23 @@ codeInterForm.className = "d-flex flex-column align-items-center";
 
 let codeInterFormInput = document.createElement('input');
 codeInterFormInput.className = "border-1 bg-transparent col-12  formsStyle";
-codeInterFormInput.style = "border-color: var(--bs-primary-200)";
+codeInterFormInput.style = "border-color: var(--bs-primary-200); direction : ltr";
 codeInterFormInput.type = "number";
 codeInterFormInput.placeholder = "- - - - - -";
 
 let codeInterFormCounter = document.createElement('p');
 codeInterFormCounter.className = "";
-//set time counter
+
 
 let codeInterFormBtn = document.createElement('button');
 codeInterFormBtn.className = "border-0 my-4 align-items-center formsStyle";
-codeInterFormBtn.style = "background-color: var(--bs-secondary-100)";
+codeInterFormBtn.style = "background-color: var(--bs-secondary-100); color : var(-bs-natural-300)";
 codeInterFormBtn.type = "submit";
 codeInterFormBtn.innerHTML = "ثبت کد";
 
 let resendCodeFormBtn = document.createElement('button');
 resendCodeFormBtn.className = "border-0 my-4 align-items-center formsStyle";
-resendCodeFormBtn.style = "background-color: var(--bs-secondary-100)";
+resendCodeFormBtn.style = "background-color: var(--bs-secondary-100); color : var(-bs-natural-300)";
 resendCodeFormBtn.type = "submit";
 resendCodeFormBtn.innerHTML = "ارسال دوباره کد";
 
@@ -62,75 +113,98 @@ codeInterForm.appendChild(codeInterFormCounter);
 codeInterForm.appendChild(codeInterFormBtn);
 codeInter.appendChild(codeInterForm);
 
-var sec = 30;
-function counter(){
-    if (sec> 0){
-        sec = sec-1 ;
-        console.log(sec);
-        codeInterFormCounter.innerHTML = sec;
-    }
-    else if(sec == 0 ){
-       codeInterForm.removeChild(codeInterFormBtn);
-       codeInterForm.appendChild(resendCodeFormBtn);
-       sec = -1;
-    }
+//________functions________
+
+//sleep function is for make a delay between executing code lines. it must be in a scope 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-codeInterFormCounter.innerHTML = sec;
 
-//first form actions: 
+//this counter is for counting seconds for code entering form
+var GlobalToggelFlag = false;
+var reqAnimFramId = null;
+var timeTillStop = 0;
+// main counter function
+function counter(){
+    timeTillStop = 31;
+    let stop = false
+    function repetedCount(){
+        sleep(1000).then(()=>{
+            timeTillStop = timeTillStop - 1;
+            if(  timeTillStop>= 0  && stop == false){
+                codeInterFormCounter.innerHTML = timeTillStop;
+                requestAnimationFrame(repetedCount);
+            }
+            else{
+                stop = true;
+                codeInterForm.removeChild(codeInterFormBtn);
+                codeInterForm.appendChild(resendCodeFormBtn);
+                GlobalToggelFlag = false;
+            }
+        });
+        
+    }
+    requestAnimationFrame(repetedCount);
+   
+}
 
-// initialise plugin
-//var iti = intlTelInput(phoneInput, {
-//utilsScript: "node_modules/intl-tel-input/build/js/utils.js?1638200991544"
-//});
+//_________first form actions__________
 
 // on click: validate
 
 phoneNumInptbtn.addEventListener('click', (e)=>{
     e.preventDefault();
     if (phoneInput.value.trim()) {
-        //if (iti.isValidNumber()) {
-        if (phoneInput.value){
+        if (phoneInput.value.length == 11){
+            phoneInput.style = "border-color: var(--bs-secondary-200);"
+            phNResponseMsg.innerHTML = "";
             validMsg.innerHTML = "✓";
             phNResponseMsg.appendChild(validMsg);
             //waite for 1 sec
-
-            //send a 6-digit number as code to the input number
-            //clear this form
-            loginPart.removeChild(loginTitrP);
-            loginPart.removeChild(phonenumForm);
-            //load form 2 : validation code form
-            loginPart.appendChild(codeInter);
-            setInterval(counter, 1000, 30);
+            sleep(2000).then(() => { 
+                //clear this form
+                loginPart.removeChild(loginTitrP);
+                loginPart.removeChild(phoneNumForm);
+                //load form 2 : validation code form
+                loginPart.appendChild(codeInter);
+                //requestAnimationFrame(update);  // start the animation;
+                console.log('befor');
+                counter();
+                console.log('after');
+            });
 
         } 
      else {
-         /*
-            input.classList.add("error");
-            let errorCode = iti.getValidationError();
-            errorMsg.innerHTML = errorMap[errorCode];
-            phNResponseMsg.appendChild(errorMsg);
-            */
+        phoneInput.style = "border-color: var(--bs-warning-100)";
+        errorMsg.innerHTML = "×";
+        phNResponseMsg.appendChild(errorMsg);
         }
     }  
 
 });
 
+
+//________second form actions_________
+
 codeInterFormBtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    location.href = "../userPanel.html";
-    //open user panel
+    if (codeInterFormInput.value.length == 6){
+        codeInterFormInput.style = "border-color: var(--bs-secondary-200); direction : ltr";
+        sleep(2000).then(() => {
+            location.href = "../userPanel.html";
+         });
+    }
+    else{
+        codeInterFormInput.style ="border-color: var(--bs-warning-100); direction : ltr"
+    }
 });
 
 resendCodeFormBtn.addEventListener('click', (e)=>{
-    //resend a new code
-
+    codeInterForm.removeChild(resendCodeFormBtn);
+    codeInterForm.appendChild(codeInterFormBtn);
+    counter();
 });
 
-/*
-function counter(){
-    sec = sec-1 ;
-    console.log(sec);
-    codeInterFormCounter.value = sec;
-}
-setInterval(counter, 1000); */
+codeInterEditeNumField.addEventListener('click', (e)=>{
+    location.reload();
+});
